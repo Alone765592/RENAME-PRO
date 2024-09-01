a97b13972c35180089cbe743b5133341a65f489c
ak4422920 <ak4422920@gmail.com>
2024-09-01 20:25:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import os, re
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", "29171167"))
 API_HASH = os.environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
-OWNER = int(os.environ.get("OWNER", "1049173662 7251898668"))
+OWNER = int(os.environ.get("OWNER", "1049173662"))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "akre_namebot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "akmoviebotz")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002210063645"))
 
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://akre:akre@cluster0.gtpo3kw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
